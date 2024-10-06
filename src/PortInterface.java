d21079ecd8cd56db183e11205eda189a16951c33
QuanAnhHoang <57248889+QuanAnhHoang@users.noreply.github.com>
2024-10-06 14:27:20 +0700

    Create PortInterface.java
    defines the operations that a Port must implement.

---
+++ src/PortInterface.java
@@ -0,0 +1,19 @@
+import java.util.List;
+
+public interface PortInterface {
+    String getId();
+    String getName();
+    double getLatitude();
+    double getLongitude();
+    int getStoringCapacity();
+    boolean hasLandingAbility();
+    int getContainerCount();
+    int getVehicleCount();
+    List<Trip> getCurrentTraffic();
+    List<Trip> getPastTraffic();
+    void addContainer(Container container);
+    void removeContainer(Container container);
+    void addVehicle(Vehicle vehicle);
+    void removeVehicle(Vehicle vehicle);
+    double calculateDistance(Port otherPort);
+}
