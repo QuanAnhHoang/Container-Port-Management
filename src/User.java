2d0a5e7012fe9a3ad9841e41833d3eb13ff5f9e6
QuanAnhHoang <57248889+QuanAnhHoang@users.noreply.github.com>
2024-10-06 14:30:15 +0700

    Update User.java
    Add implements Serializable to allow for serialization.
    
    Addprivate static final long serialVersionUID = 1L; for version control in serialization.
    
    Chang processOperation method signature to include PortManagementSystem system parameter.

--- src/User.java
+++ src/User.java
@@ -1,7 +1,10 @@
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public abstract class User {
+public abstract class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
     protected String username;
     protected String password;
     protected List<String> permissions;
@@ -24,5 +27,5 @@ public abstract class User {
 
     public abstract void displayMenu();
 
-    public abstract void processOperation(String operation);
+    public abstract void processOperation(String operation, PortManagementSystem system);
 }
