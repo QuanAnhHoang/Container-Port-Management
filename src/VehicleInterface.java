1ada0157d2355914755b3f9bf99500dc18bb9d01
QuanAnhHoang <57248889+QuanAnhHoang@users.noreply.github.com>
2024-10-06 14:27:36 +0700

    Create VehicleInterface.java
    defines the operations that a Vehicle must implement.

---
+++ src/VehicleInterface.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+public interface VehicleInterface {
+    String getId();
+    String getName();
+    double getCurrentFuel();
+    double getFuelCapacity();
+    int getCarryingCapacity();
+    Port getCurrentPort();
+    void setCurrentPort(Port port);
+    List<Container> getContainers();
+    void addContainer(Container container);
+    void removeContainer(Container container);
+    boolean canMove(Port destination);
+    void move(Port destination);
+    void refuel(double amount);
+}
